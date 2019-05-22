f464d48eda8b46769b19a4278a1cdd78ff569ccf
unknown <felixlopezarm1@gmail.com>
2019-05-22 13:33:15 -0400

    :tada: Commit Inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Home Pet App API Backend
+
+## Instalacion
+```sh
+  cd homepet-backend
+  npm install
+  npm run dev 
+  # El servidor Corre en http://localhost:3000
+```
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/' , (req, res) =>{
+  res.json({
+    msg: 'Hello World Home Pet'
+  })
+})
+
+module.exports = app;
