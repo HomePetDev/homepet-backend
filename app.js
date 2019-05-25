e48d7915a072fe470d9d429729315f5b89577fa2
unknown <felixlopezarm1@gmail.com>
2019-05-24 21:06:58 -0400

    Login y Registro de usuarios

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv/config');
 
+// Importando rutas
+const auth = require('./routes/auth'); 
+const users = require('./routes/api');
+// Importando middlewares
+const { notFound , errorHandler } = require('./middlewares');
 
 const app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
 app.get('/' , (req, res) =>{
   res.json({
     msg: 'Hello World Home Pet'
   })
 })
+
+
+app.use('/api', users);
+app.use('/api/auth', auth);
+
+app.use(notFound);
+app.use(errorHandler)
+
 
 module.exports = app;
