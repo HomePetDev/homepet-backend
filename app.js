e1879488a2939df067dd52e329b558f4c3b83b45
unknown <felixlopezarm1@gmail.com>
2019-06-13 19:48:37 -0400

    CORS middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv/config');
 
 // Importando rutas
@@ -13,6 +14,7 @@ const app = express();
 
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
